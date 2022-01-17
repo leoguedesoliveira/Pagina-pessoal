83b6177d421235665b3a6493cb0e7b480b646011
leoguedesoliveira <leonardoguedes.dasilvaguedesol@gmail.com>
2022-01-17 19:42:05 -0300

    mudando menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnMobile = document.getElementById('buttonMenu')
+
+function toggleMenu(){
+    const btnMobile = document.getElementById('menuzin')
+    menuzin.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
